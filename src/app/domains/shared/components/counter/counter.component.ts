ac44ab44c1f9ce98733a8fd007adb8a2d4532529
omarsmarinhos <omarmarinosaguilar@gmail.com>
2024-03-09 20:39:04 -0500

    Components life cycles

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -8,20 +8,43 @@ import { Component, Input, SimpleChanges } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
-  @Input({required: true}) duration: number = 0;
-  @Input({required: true}) message: string = '';
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
 
   constructor() {
-    //NO ASYNC
-    //BEFORE RENDER
-    console.log("Constructor")
-    console.log("-".repeat(10))
+    // NO ASYNC
+    // before render
+    // una vez
+    console.log('constructor');
+    console.log('-'.repeat(10));
   }
-  
-  ngOnChange(changes: SimpleChanges) {
-    //BEFORE RENDER AND DURING
-    console.log("ngOnChange")
-    console.log(changes)
-    console.log("-".repeat(10))
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    // after render
+    // una vez
+    // async, then, subs
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // hijos ya fueron pintados
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
   }
 }
