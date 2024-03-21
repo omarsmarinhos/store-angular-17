abc81c1739963f321c2ca9e8cff5c8009d10942e
omarsmarinhos <omarmarinosaguilar@gmail.com>
2024-03-21 00:17:31 -0500

    Calc Total

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 
@@ -12,9 +12,22 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
 
   hideSideMenu = signal(true);
+  total = signal(0);
+
   @Input({required: true}) cart: Product[] = [];
 
   toggleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState)
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if (cart) {
+      this.total.set(this.calcTotal());
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 }
