83180903ec9391c5d9af1c321bb21e5d5f70c3c5
omarsmarinhos <omarmarinosaguilar@gmail.com>
2024-09-05 18:55:02 -0500

    Add Pipe's

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -3,5 +3,5 @@ export interface Product {
     title: string,
     price: number,
     images: string[],
-    createdAt: string
+    creationAt: string
 }
