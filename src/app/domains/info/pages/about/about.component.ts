4dcbeca58f45388a7869003d9d5e3e054e5e99ed
omarsmarinhos <omarmarinosaguilar@gmail.com>
2024-09-05 19:25:05 -0500

    Add my directive

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { CommonModule } from '@angular/common';
+import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterComponent, CommonModule],
+  imports: [CounterComponent, CommonModule, HighlightDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
